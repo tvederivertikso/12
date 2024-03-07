67e3085ab4ce00d5770900b6808fe31435b62cec
tvederivertikso <kakuzurim@gmail.com>
2024-03-07 09:26:13 +0800

    ttttttt

--- 12/Program.cs
+++ 12/Program.cs
@@ -14,7 +14,7 @@ namespace calculator
             float one, two, result;
             char sign;
             Console.WriteLine("Добро пожаловать в калькулятор. Вам нелбходимо ввести первое число, затем знак действия(+,-,*,/), которое хотите совершить и второе число.");
-            Console.Write("Введите первое число: ");
+            Console.Write("Введите первое число: ")   ;
             one = Convert.ToSingle(Console.ReadLine()) ;
             Console.Write("Введите знак действия : ");
             sign = Convert.ToChar(Console.ReadLine()) ;
